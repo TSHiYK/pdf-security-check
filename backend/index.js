66057a41431ce28c383a4341d5794d2dcb32e379
TSHiYK <toshiyuki.i1223+dev@gmail.com>
2024-06-07 10:58:49 +0900

    fix: index.js

--- backend/index.js
+++ backend/index.js
@@ -23,9 +23,9 @@ app.use('/api', routes);
 app.set('io', io);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'dist')));
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+        res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
     });
 }
 
